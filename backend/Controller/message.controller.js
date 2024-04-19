5845a13eb36f4419103b04bd4603b645983261c9
riteshjk <96072567+riteshjk@users.noreply.github.com>
2024-04-19 12:32:36 +0530

    able to do the two way conversation

--- backend/Controller/message.controller.js
+++ backend/Controller/message.controller.js
@@ -29,8 +29,10 @@ export const sendMessage = async (req, res) => {
         if(new_message){
             conversation.messages.push(new_message._id)
         }
-        await conversation.save();
-        await new_message.save()
+        // await conversation.save();
+        // await new_message.save()
+
+        await Promise.all([conversation.save(),new_message.save()])
         res.status(201).json(new_message)
     }
     catch (error){
@@ -38,4 +40,27 @@ export const sendMessage = async (req, res) => {
 };
 
    
+}
+
+export const getMessage = async(req,res,next)=>{
+  try{
+    const {id:userToChatId} = req.params;
+    const senderId = req.user._id ;
+
+    const conversation = await Conversation.findOne({
+        participants : {$all : [senderId,userToChatId]}
+    }).populate("messages");
+
+    if(!conversation){
+        return res.status(200).json([])
+    }
+    const messages = conversation.messages;
+
+     res.status(200).json(messages)
+ 
+  }
+  catch (error){
+    return res.status(500).json({ message: error.message });
+ };
+  
 }
--- backend/Routes/message.route.js
+++ backend/Routes/message.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { sendMessage } from "../Controller/message.controller.js";
+import { getMessage, sendMessage } from "../Controller/message.controller.js";
 import { ProtectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post("/send/:id",ProtectRoute,sendMessage)
+router.get("/:id",ProtectRoute,getMessage)
+
 
 export default router;
